c348409767ca32b392fccd478e995e313e6ba951
André <ni.mailix@gmail.com>
2023-06-11 19:36:57 +0300

    11/06/2023 19:36

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,7 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  styles: [
+    `
+    .app-title-login {
+      text-align: center;
+      padding: 10px;
+      color: #333;
+      width: 500px;
+      align : center;
+    }
+    `
+  ]
 })
 export class LoginComponent {
 
